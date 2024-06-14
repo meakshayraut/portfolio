9aa7097ae36cd7cbefa586a2926b18aac051418f
meakshayraut <akshayrautg@gmail.com>
2024-06-14 15:41:36 +0530

    Updates

---
+++ precache-manifest.f1c179fd650753f9b9aff3d5355ee54f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3814a6120d6afa0104028e3cbb81f2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "07737c1bdb5ff8430c23",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "d8f73cb2caab7cd53fd5",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "1616f128f3ab3ed77ec3",
+    "url": "/portfolio/static/css/main.e40afea7.chunk.css"
+  },
+  {
+    "revision": "9bf25dd976a08fd6bc0e",
+    "url": "/portfolio/static/js/2.43c41047.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/portfolio/static/js/2.43c41047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07737c1bdb5ff8430c23",
+    "url": "/portfolio/static/js/3.1037dc21.chunk.js"
+  },
+  {
+    "revision": "d8f73cb2caab7cd53fd5",
+    "url": "/portfolio/static/js/4.0a398bfe.chunk.js"
+  },
+  {
+    "revision": "1616f128f3ab3ed77ec3",
+    "url": "/portfolio/static/js/main.e1e3b6f4.chunk.js"
+  },
+  {
+    "revision": "0d01751b991c39ab613e",
+    "url": "/portfolio/static/js/runtime-main.d8f51522.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59763beb8c9a328b564471e0c8e99a26",
+    "url": "/portfolio/static/media/coursera-logo.59763beb.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5e12d92d37dc6ac02d2bf9c5e7488a4f",
+    "url": "/portfolio/static/media/gpalogo.5e12d92d.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
